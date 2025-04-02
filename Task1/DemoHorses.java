8ee990b8b5f6d615180dccbd3d08a7ef243fb4ae
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-02 12:38:32 +0200

    getting user input to define horse

--- Task1/DemoHorses.java
+++ Task1/DemoHorses.java
@@ -1,5 +1,22 @@
 package javainheritance.Task1;
 
+import java.util.Scanner;
+
 public class DemoHorses {
-    
+    public static void main(String[] args) {
+        // Defining a new horse
+        Scanner input = new Scanner(System.in);
+        Horse hest = new Horse();
+
+        System.out.println("Please enter the name of the horse: ");
+        hest.setName(input.nextLine());
+
+        System.out.println("Please enter the color of the horse: ");
+        hest.setColor(input.nextLine());
+
+        System.out.println("Please enter the birth year of the horse: ");
+        hest.setBirthYear(input.nextInt());
+
+        
+    }
 }
