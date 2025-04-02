975d913f886968e7ea3ec1ce18146a4d97b31675
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-04-02 12:24:27 +0200

    added getters and setters to horse

--- Task1/Horse.java
+++ Task1/Horse.java
@@ -1,9 +1,34 @@
 package javainheritance.Task1;
 
 public class Horse {
+    // Data fields for horse values
     private String name;
     private String color;
     private int birthYear;
 
-    
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
 }
